9731c7213ae0254707bf4c0d761507bc3629ca30
minsikzzang <minsikzzang@hotmail.com>
2011-10-27 15:43:56 +0000

    server recipes added

--- cookbooks/server/metadata.rb
+++ cookbooks/server/metadata.rb
@@ -4,3 +4,10 @@ license          "Apache 2.0"
 description      "Installs/Configures server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe           "server", "Loads servers databags and selects recipes to use"
+recipe           "server::ubuntu", "Setups up based ubuntu box"
+
+%w{ apt ntp perl munin monit java vsftpd user}.each do |cb|
+  depends cb
+end
