81a607dbfca5933781999ae93903a9ea7c947276
Ubuntu <ubuntu@ip-10-58-73-22.eu-west-1.compute.internal>
2011-10-27 12:28:38 +0000

    some added

---
+++ .chef/knife.rb
@@ -0,0 +1,10 @@
+log_level                :info
+log_location             STDOUT
+node_name                'timejust'
+client_key               '/etc/chef/timejust.pem'
+validation_client_name   'timejust-validator'
+validation_key           '/etc/chef/validation.pem'
+chef_server_url          'http://chef.timejust.com:4000'
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
