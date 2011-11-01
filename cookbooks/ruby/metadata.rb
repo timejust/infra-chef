8d3fe086c71575a214406a5619d68d9335c2e46b
minsikzzang <minsikzzang@hotmail.com>
2011-11-01 22:48:38 +0000

    ruby added

--- cookbooks/ruby/metadata.rb
+++ cookbooks/ruby/metadata.rb
@@ -4,7 +4,6 @@ license          "Apache 2.0"
 description       "Installs Ruby 1.9.2 from source"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "1.0"
-supports          ubuntu
 
 %w( apt build-essential ).each do |d|
   depends d
