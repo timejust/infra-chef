50d90d2d111074fed8ab2290bab980fd0b60d826
minsikzzang <minsikzzang@hotmail.com>
2011-11-03 15:55:50 +0000

    initial hudson receipt added

---
+++ cookbooks/tool/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "timejust.com"
+maintainer_email "minsik.kim@timejust.com"
+license          "Apache 2.0"
+description      "Installs/Configures tool"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+%w{ apt runit user deploy nginx java monit hudson }.each do |cb|
+  depends cb
+end
---
+++ cookbooks/tool/recipes/default.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: tool
+# Recipe:: default
+#
+# Copyright 2011, timejust.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "deploy"
+include_recipe "apt"
+
+
+node.run_state[:tools] = []
+
+search(:tools) do |tool|
+  (app["server_roles"] & node.run_list.roles).each do |tool_role|
+    node.run_state[:apps] << {:app => app, :recipes => app["type"][app_role]}
+  end
+end
+
+node.run_state[:tools].map {|a| a[:recipes]}.flatten.each do |recipe|
+  ## Do this so that different databaags with the same recipe can run
+  node.run_state[:seen_recipes].delete("tool::#{recipe}")
+  include_recipe "tool::#{recipe}"
+end
+
+
+node.run_state.delete(:tools)
+
