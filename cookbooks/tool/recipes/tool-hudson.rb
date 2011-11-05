b5c10853d21eb935e96ffa01c009f684e2173502
minsikzzang <minsikzzang@hotmail.com>
2011-11-05 03:04:42 +0100

    added a lot

--- cookbooks/tool/recipes/tool-hudson.rb
+++ cookbooks/tool/recipes/tool-hudson.rb
@@ -20,8 +20,19 @@ node.run_state[:tools].each do |current_tool|
     action :start
   end
   
+  ## Next, install any application specific gems
+  if tool['gems']
+    tool['gems'].each do |gem,ver|
+      gem_package gem do
+        action :install
+        version ver if ver && ver.length > 0
+      end
+    end
+  end
+  
   ## Create ssh key so hudson can pull from github 
-  directory "#{node[:hudson][:server][:home]}/.ssh" do
+  # directory "#{node[:hudson][:server][:home]}/.ssh" do
+  directory "/home/#{tool[:owner]}/.ssh" do
     action :create
     mode 0755
     owner tool[:owner]
@@ -29,7 +40,8 @@ node.run_state[:tools].each do |current_tool|
   
   ruby_block "hudson_deploy_keys" do
     block do
-      f = File.open("#{tool[:deploy_to]}/.ssh/id_rsa", "w")
+      #f = File.open("#{tool[:deploy_to]}/.ssh/id_rsa", "w")
+      f = File.open("/home/#{tool[:owner]}/.ssh/id_rsa", "w")
       f.puts("#{tool[:deploy_key]}")
       f.close
     end
@@ -38,15 +50,16 @@ node.run_state[:tools].each do |current_tool|
   bash "change permissions" do
     user "root"
     code <<-EOH
-      chown #{tool[:owner]} #{tool[:deploy_to]}/.ssh/id_rsa
-      chmod 0600 #{tool[:deploy_to]}/.ssh/id_rsa
+      chown #{tool[:group]}:#{tool[:owner]} /home/#{tool[:owner]}/.ssh/id_rsa
+      chmod 0600 /home/#{tool[:owner]}/.ssh/id_rsa
     EOH
   end  
     
   ## Create ssh forwarding so hudson can use its key to pull from git remotely
-  cookbook_file "#{tool[:deploy_to]}/.ssh/config" do
+  cookbook_file "/home/#{tool[:owner]}/.ssh/config" do
     source      "#{tool[:id]}/config"
     owner       tool[:owner]
+    group       tool[:group]
     mode        0600
   end 
   
